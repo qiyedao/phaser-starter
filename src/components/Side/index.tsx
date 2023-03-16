4b7e2a854b8dcb4c28ab9324805dca69ee58f88a
qiye <1710797241@qq.com>
2023-03-16 17:04:55 +0800

    chore: 添加tailwind styled-components

--- src/components/Side/index.tsx
+++ src/components/Side/index.tsx
@@ -2,14 +2,21 @@ import { Button } from 'antd-mobile';
 import classNames from 'classnames';
 import React from 'react';
 import styles from './index.module.less';
+import styled from 'styled-components';
 interface ISideProps {
     width: number;
 }
+const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+`;
 const Side: React.FC<ISideProps> = ({ width }) => {
     return (
         <div className={classNames(styles.side, ' bg-red-400')} style={{ width }}>
-            大屏侧边栏
-            <Button color="primary">click</Button>
+            <Wrapper>
+                大屏侧边栏
+                <Button color="primary">click</Button>
+            </Wrapper>
         </div>
     );
 };
